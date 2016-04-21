44da57abe13789ead0ac1d9e84b8c682e42114bb
William R. Fraser <wfraser@codewise.org>
2016-04-20 23:25:31 -0700

    move the example program to its own directory and give it its own Cargo.toml file so it's totally independent

---
+++ src/libc_wrappers.rs
@@ -0,0 +1,109 @@
+// Libc Wrappers :: Safe wrappers around system calls.
+//
+// Copyright (c) 2016 by William R. Fraser
+//
+
+use std::ffi::{CString, OsString};
+use std::io;
+use std::mem;
+use std::ptr;
+use std::os::unix::ffi::OsStringExt;
+use libc;
+
+pub fn opendir(path: OsString) -> Result<u64, libc::c_int> {
+    let path_c = match CString::new(path.into_vec()) {
+        Ok(s) => s,
+        Err(e) => {
+            error!("opendir: path {:?} contains interior NUL byte",
+                   OsString::from_vec(e.into_vec()));
+            return Err(libc::EINVAL);
+        }
+    };
+
+    let dir: *mut libc::DIR = unsafe { libc::opendir(mem::transmute(path_c.as_ptr())) };
+    if dir.is_null() {
+        return Err(io::Error::last_os_error().raw_os_error().unwrap());
+    }
+
+    Ok(dir as u64)
+}
+
+pub fn readdir(fh: u64) -> Result<Option<libc::dirent>, libc::c_int> {
+    let dir: *mut libc::DIR = unsafe { mem::transmute(fh) };
+    let mut entry: libc::dirent = unsafe { mem::zeroed() };
+    let mut result: *mut libc::dirent = ptr::null_mut();
+
+    let error: i32 = unsafe { libc::readdir_r(dir, &mut entry, &mut result) };
+    if error != 0 {
+        return Err(error);
+    }
+
+    if result.is_null() {
+        return Ok(None);
+    }
+
+    Ok(Some(entry))
+}
+
+pub fn closedir(fh: u64) -> Result<(), libc::c_int> {
+    let dir: *mut libc::DIR = unsafe { mem::transmute(fh) };
+    if -1 == unsafe { libc::closedir(dir) } {
+        Err(io::Error::last_os_error().raw_os_error().unwrap())
+    } else {
+        Ok(())
+    }
+}
+
+pub fn open(path: OsString, flags: libc::c_int) -> Result<u64, libc::c_int> {
+    let path_c = match CString::new(path.into_vec()) {
+        Ok(s) => s,
+        Err(e) => {
+            error!("open: path {:?} contains interior NUL byte",
+                   OsString::from_vec(e.into_vec()));
+            return Err(libc::EINVAL);
+        }
+    };
+
+    let fd: libc::c_int = unsafe { libc::open(mem::transmute(path_c.as_ptr()), flags) };
+    if fd == -1 {
+        return Err(io::Error::last_os_error().raw_os_error().unwrap());
+    }
+
+    Ok(fd as u64)
+}
+
+pub fn close(fh: u64) -> Result<(), libc::c_int> {
+    let fd = fh as libc::c_int;
+    if -1 == unsafe { libc::close(fd) } {
+        Err(io::Error::last_os_error().raw_os_error().unwrap())
+    } else {
+        Ok(())
+    }
+}
+
+pub fn lstat(path: OsString) -> Result<libc::stat64, libc::c_int> {
+    let path_c = match CString::new(path.into_vec()) {
+        Ok(s) => s,
+        Err(e) => {
+            error!("lstat: path {:?} contains interior NUL byte",
+                   OsString::from_vec(e.into_vec()));
+            return Err(libc::EINVAL);
+        }
+    };
+
+    let mut buf: libc::stat64 = unsafe { mem::zeroed() };
+    if -1 == unsafe { libc::lstat64(mem::transmute(path_c.as_ptr()), &mut buf) } {
+        return Err(io::Error::last_os_error().raw_os_error().unwrap());
+    }
+
+    Ok(buf)
+}
+
+pub fn fstat(fd: u64) -> Result<libc::stat64, libc::c_int> {
+    let mut buf: libc::stat64 = unsafe { mem::zeroed() };
+    if -1 == unsafe { libc::fstat64(fd as libc::c_int, &mut buf) } {
+        return Err(io::Error::last_os_error().raw_os_error().unwrap());
+    }
+
+    Ok(buf)
+}
---
+++ src/main.rs
@@ -0,0 +1,55 @@
+// Main Entry Point :: A fuse_mt test program.
+//
+// Copyright (c) 2016 by William R. Fraser
+//
+
+use std::env;
+use std::ffi::{OsStr, OsString};
+
+extern crate fuse;
+extern crate libc;
+extern crate time;
+
+#[macro_use]
+extern crate log;
+
+extern crate fuse_mt;
+
+mod libc_extras;
+mod libc_wrappers;
+mod passthrough;
+
+struct ConsoleLogger;
+
+impl log::Log for ConsoleLogger {
+    fn enabled(&self, _metadata: &log::LogMetadata) -> bool {
+        //metadata.level() >= log::LogLevel::Debug
+        true
+    }
+
+    fn log(&self, record: &log::LogRecord) {
+        println!("{}: {}: {}", record.target(), record.level(), record.args());
+    }
+}
+
+fn main() {
+    log::set_logger(|max_log_level| {
+        max_log_level.set(log::LogLevelFilter::Debug);
+        Box::new(ConsoleLogger)
+    }).unwrap();
+
+    let args: Vec<OsString> = env::args_os().collect();
+
+    if args.len() != 3 {
+        println!("usage: {} <target> <mountpoint>", &env::args().next().unwrap());
+        ::std::process::exit(-1);
+    }
+
+    let filesystem = passthrough::PassthroughFS {
+        target: args[1].clone(),
+    };
+
+    let fuse_args: Vec<&OsStr> = vec![&OsStr::new("-o"), &OsStr::new("auto_unmount")];
+
+    fuse::mount(fuse_mt::FuseMT::new(filesystem, 1), &args[2], &fuse_args);
+}
