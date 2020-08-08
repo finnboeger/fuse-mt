71f95201f71419cecfde77c32d2fc93213433d10
Finn Böger <finnboeger@googlemail.com>
2020-08-08 11:24:17 +0200

    add search

--- src/cache.rs
+++ src/cache.rs
@@ -1,48 +1,117 @@
-use walkdir::WalkDir;
+use std::cmp::Ordering;
 use std::path::Path;
+use walkdir::WalkDir;
 
+#[derive(Debug)]
 pub enum Entry {
-    Dict { name: String, contents: Vec<Box<Entry>> },
+    Dict { name: String, contents: Vec<Entry> },
     File(String),
 }
 
 impl Entry {
-    fn add_entry(&mut self, path: &Path) -> Result<(), &str> {
-        match self {
-            Entry::File(_) => Err("can't add entry to a file"),
-            Entry::Dict{ name, contents } => {
-                let name = String::from(path.file_name()?.to_str()?);
-                if path.is_dir() {
-                    contents.push(Box::new(Entry::Dict {
-                        name,
-                        contents: Vec::new(),
-                    }))
-                } else {
-                    contents.push(Box::new(Entry::File(name)))
-                }
-            },
+    pub fn new(path: &Path) -> Self {
+        // TODO: Error handling if either path has no file_name or can't be converted to string.
+        //       Maybe change Entry to use OsStr?
+        let name = String::from(
+            path.file_name()
+                .ok_or("no file name")
+                .unwrap()
+                .to_str()
+                .ok_or("invalid file name")
+                .unwrap(),
+        );
+        if path.is_dir() {
+            Entry::Dict {
+                name,
+                contents: Vec::new(),
+            }
+        } else {
+            Entry::File(name)
         }
-        Ok(())
     }
 
-    fn find(&self, path: &Path) -> Result<&Entry, &str> {
-        todo!("implement search");
-        Ok(self)
+    fn add_entry(&mut self, path: &Path) -> Result<(), &str> {
+        //println!("adding {}, is dir: {}", path.display(), path.is_dir());
+        match self {
+            Entry::File(_) => Err("can't add entry to a file"),
+            Entry::Dict { name, contents } => {
+                contents.push(Entry::new(path));
+                Ok(())
+            }
+        }
+    }
+
+    fn find(&mut self, path: &Path) -> Result<&mut Entry, &str> {
+        if path == Path::new("") {
+            return Ok(self);
+        }
+        //println!("path: {}", path.display());
+        let mut ancestors: Vec<&Path> = path.ancestors().collect();
+        //println!("Ancestors: {:?}", ancestors);
+        // Drop last two ancestors which are the root element ('') and '.'
+        ancestors.pop();
+        ancestors.pop();
+        ancestors.reverse();
+
+        let mut item = Ok(self);
+        for ancestor in ancestors {
+            //println!("Searching for {}, is_dir: {}", ancestor.display(), ancestor.is_dir());
+            match item? {
+                Entry::File(_) => item = Err("can't search in a file"),
+                Entry::Dict { name, contents } => {
+                    // We're assuming that all Entries are sorted, therefore we can execute a binary search.
+                    item = match contents.binary_search_by(|other: &Entry| -> Ordering {
+                        let a = ancestor.file_name().unwrap().to_str().unwrap();
+                        let b = match other {
+                            Entry::File(s) => s,
+                            Entry::Dict{ name, contents } => name,
+                        };
+                        // TODO: solve File not Found error when it obviously exists
+                        //println!("contents: {:#?}", contents);
+                        //println!("a: {}, b: {}, cmp: {:?}", a, b, a.cmp(b));
+                        b.cmp(&String::from(a))
+                    }) {
+                        Ok(i) => Ok(&mut contents[i]),
+                        Err(_) => Err("File not found"),
+                    };
+                }
+            }
+        }
+        item
     }
 }
 
 //TODO: Error handling
 pub fn build(path: &str) {
+
+    // TODO: assert path is a directory
+    let working_dir = std::env::current_dir().unwrap();
+
+    // Create root
     let mut root = Entry::Dict {
         name: String::from("."),
         contents: Vec::new(),
     };
-    for entry in WalkDir::new(path) {
-        match entry.unwrap().path().strip_prefix(path) {
-            Ok(p) => {
-                root.find(p.parent().unwrap()).unwrap().add_entry(p);
-            },
-            Err(_) => {}
-        }
+
+    std::env::set_current_dir(path).unwrap();
+    let entries = WalkDir::new(".")
+        .sort_by(|a, b| a.file_name().cmp(b.file_name()))
+        .min_depth(1);
+
+    for entry in entries {
+        let e = entry.unwrap();
+        let p = e.path();
+        //println!("full_path: {}", p.display());
+
+        // For a file to be added, the parent has to have been added first so unwrapping should be safe.
+        let parent = match p.parent() {
+            None => &mut root,
+            Some(x) => root.find(x).unwrap(),
+        };
+        &parent.add_entry(p).unwrap();
     }
+    println!("{:#?}", root);
+
+    // Restore original working directory
+    std::env::set_current_dir(working_dir).unwrap();
 }
