97478cc50282ea191e64b532f1c6d1fd43ba22dd
Finn Böger <finnboeger@googlemail.com>
2020-08-13 22:21:05 +0200

    fix more warnings

--- src/file_handles.rs
+++ src/file_handles.rs
@@ -2,7 +2,6 @@ use anyhow::{anyhow, Result};
 use std::collections::HashMap;
 use std::path::PathBuf;
 use std::io::Cursor;
-use std::path::Path;
 use std::ffi::OsString;
 
 pub struct FileHandles {
